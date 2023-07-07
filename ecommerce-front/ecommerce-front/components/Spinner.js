14c703090025cb873701f2ef95bb00e18b027658
mohammed <khrishi_mohammed@yahoo.com>
2023-07-07 21:19:27 +0300

    final push

---
+++ ecommerce-front/ecommerce-front/components/Spinner.js
@@ -0,0 +1,20 @@
+import {BounceLoader, GridLoader} from "react-spinners";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  ${props => props.fullWidth ? `
+    display:block;
+    display:flex;
+    justify-content:center;
+  ` : `
+    border: 5xp solid blue;
+  `}
+`;
+
+export default function Spinner({fullWidth}) {
+  return (
+    <Wrapper fullWidth={fullWidth}>
+      <GridLoader speedMultiplier={3} color={'#36d7b7'} />
+    </Wrapper>
+  );
+}
---
+++ ecommerce-front/ecommerce-front/components/WhiteBox.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const WhiteBox = styled.div`
+  background-color: #fff;
+  box-shadow: 5.6px 12.1px 10px rgba(0, 0, 0, 0.035),
+  45px 97px 80px rgba(0, 0, 0, 0.07);
+  border-radius: 10px;
+  padding: 30px;
+`;
+
+export default WhiteBox;
